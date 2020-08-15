495ae0ec3de17b42f4059a510b5ea0bb0c6a5b1d
Minki96 <46333497+Minki96@users.noreply.github.com>
2020-08-15 12:32:41 +0900

    version1.1.0
    made by TaeHwan

--- Minki/video.py
+++ Minki/video.py
@@ -1,10 +1,6 @@
-# import plaidml.keras
-# plaidml.keras.install_backend()
 import time
 import datetime
 import os
-# os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-
 from requests import request
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.models import load_model
@@ -12,13 +8,13 @@ import numpy as np
 import cv2
 from pyimagesearch.centroidtracker import CentroidTracker
 
-# from cloud_messaging import mains
+# from cloud_messaging import mainss
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 now = datetime.datetime.now()
 facenet = cv2.dnn.readNet('models/deploy.prototxt', 'models/res10_300x300_ssd_iter_140000.caffemodel')
 model = load_model('models/fourB_detect_mask.model')
 ct = CentroidTracker()
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cap = cv2.VideoCapture(0)
 
 print(f'Video시작: {now.year}년 {now.month}월 {now.day}일 {now.hour}시 {now.minute}분 {now.second}초')
 
@@ -37,7 +33,6 @@ while True:
     dets = facenet.forward()
     result_img = img.copy()
     result_img = cv2.cvtColor(result_img, cv2.COLOR_BGR2RGB)
-    
     rects = []
 
     for i in range(dets.shape[2]):
@@ -49,44 +44,70 @@ while True:
             face = img[y1:y2, x1:x2]
             box = dets[0, 0, i, 3:7] * np.array([w, h, w, h])
             rects.append(box.astype("int"))
-            try:
-                face_input = cv2.resize(face, dsize=(224, 224))
-                face_input = cv2.cvtColor(face_input, cv2.COLOR_BGR2RGB)
-                face_input = preprocess_input(face_input) 
-                face_input = np.expand_dims(face_input, axis=0)
-                mask, no_mask = model.predict(face_input).squeeze()
+            
+            area = (box[2] - box[0]) * (box[3] - box[1])
+            area_under = 50000 ; area_upper = 80000
+            mask_rate = 0.7
+            print(area)
+            
+            if area_under < area < area_upper :
+                try:
+                    face_input = cv2.resize(face, dsize=(224, 224))
+                    face_input = cv2.cvtColor(face_input, cv2.COLOR_BGR2RGB)
+                    face_input = preprocess_input(face_input) 
+                    face_input = np.expand_dims(face_input, axis=0)
+                    mask, no_mask = model.predict(face_input).squeeze()
 
-                if mask > no_mask:
-                    color = (0, 255, 0)
-                    label = 'Mask %d%%' % (mask * 100)
+                except:
+                    continue
+                
+            elif area >= area_upper :
+                try:
+                    face_input = cv2.resize(face, dsize=(224, 224))
+                    face_input = cv2.cvtColor(face_input, cv2.COLOR_BGR2RGB)
+                    face_input = preprocess_input(face_input) 
+                    face_input = np.expand_dims(face_input, axis=0)
+                    mask, no_mask = model.predict(face_input).squeeze()
 
-                elif no_mask > 0.6:
-                    color = (0, 0, 255)
-                    label = 'No Mask %d%%' % (no_mask * 100)
+                    if mask > mask_rate :
+                        color = (0, 255, 0)
+                        label = 'Mask %d%%' % (mask * 100)
+    
+                    else :
+                        color = (0, 0, 255)
+                        label = 'No Mask %d%%' % (no_mask * 100)
 
-            except:
+                except:
+                    continue
+                
+                cv2.rectangle(result_img, pt1=(x1, y1), pt2=(x2, y2), thickness=2, color=color, lineType=cv2.LINE_AA)
+                cv2.putText(result_img, text=label, org=(x1, y1 - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color= color , thickness=2, lineType=cv2.LINE_AA)
+                
+                people_count = []
+                objects = ct.update(rects)
+                
+                try:
+                    
+                    for (objectID, centroid) in objects.items():
+                        text = "ID{}".format(objectID)
+                        if text not in Id_list:
+                            Id_list.append(text)
+                            if color == (0, 0, 255):
+                                print('request..', text)
+                                # cap_img = cv2.imwrite('./no_mask_person/no_mask_person' + text +'.jpg', result_img)
+
+
+                        cv2.putText(result_img, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),2)
+                        cv2.circle(result_img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+                        people_count.append(objectID)
+                        
+                except:
+                    continue
+                
+            else :
                 continue
-        cv2.rectangle(result_img, pt1=(x1, y1), pt2=(x2, y2), thickness=2, color=color, lineType=cv2.LINE_AA)
-        cv2.putText(result_img, text=label, org=(x1, y1 - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color= color , thickness=2, lineType=cv2.LINE_AA)
 
-    people_count = []
-    objects = ct.update(rects)
-    try:
-        for (objectID, centroid) in objects.items():
-            text = "ID{}".format(objectID)
-            if text not in Id_list:
-                Id_list.append(text)
-                if color == (0, 0, 255):
-                    print('request..', text)
-                    cap_img = cv2.imwrite('./no_mask_person/no_mask_person' + text +'.jpg', result_img)
-                    # request.post('http://3.34.183.253:5000/nomask')
-
-
-            cv2.putText(result_img, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),2)
-            cv2.circle(result_img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
-            people_count.append(objectID)
-    except:
-        continue
+    
     cv2.imshow('result', result_img)
     if cv2.waitKey(1) == ord('q'): 
         cv2.destroyAllWindows()
@@ -94,6 +115,8 @@ while True:
 
 # people_cnt = people_count.pop() + 1
 # print('인원수:', people_cnt, '명')
+
+
 cap.release()
 e_time = time.time()
 now = datetime.datetime.now()
